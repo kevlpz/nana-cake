54f24ccd5382eb0bfa7445ce06aa797fe6ab5c67
TheHaunted <kevin_lopez@live.com>
2020-06-22 01:38:36 -0500

    adds side-menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import { GoThreeBars } from "react-icons/go";
 
 const Header = () => {
     return (
         <div className="header-container">
-            <img src={logo} className="logo" alt="Nana Logo"/>
+            <img src={logo} className="logo" alt="Nana Logo" />
+            <div className="menu-button" onClick={() => console.log('clicked!')}>
+                <GoThreeBars />
+            </div>
         </div>
     )
 }
