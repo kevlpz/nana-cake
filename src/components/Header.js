08949a6c5aae460f9692a279721d94f7595761f3
TheHaunted <kevin_lopez@live.com>
2020-06-24 04:29:12 -0500

    adds navbar links and mobile-view collapse

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import logo from '../assets/logo.png';
 import { GoThreeBars } from "react-icons/go";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const Header = () => {
     return (
-        <div className="header-container">
-            <img src={logo} className="logo" alt="Nana Logo" />
-
-            <div className="menu-button" onClick={() => console.log('clicked!')}>
-                <GoThreeBars />
-            </div>
-        </div>
+        <Navbar className="navbar" expand="lg">
+            <Navbar.Brand>
+                <img src={logo} className="logo" alt="Nana Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle className="nav-toggle"><GoThreeBars /></Navbar.Toggle>
+            <Navbar.Collapse>
+                <Nav className="ml-auto">
+                    <Nav.Link href="#products">Products</Nav.Link>
+                    <Nav.Link href="#comissions">Comissions</Nav.Link>
+                    <Nav.Link href="#blog">Blog</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
