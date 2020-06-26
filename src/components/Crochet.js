bd285ac0f09f518804f8ad3bd3cb4b8959252a57
TheHaunted <kevin_lopez@live.com>
2020-06-26 18:16:56 -0500

    creates crochet component

--- src/components/Crochet.js
+++ src/components/Crochet.js
@@ -1,8 +1,130 @@
 import React from 'react';
+import ContentCard from './ContentCard';
+import logo from '../assets/logo.png';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const Crochet = () => {
+const content = [
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+    {
+        title: 'crochet',
+        img: logo,
+        url: 'crochet-link'
+    },
+]
+
+const Crochet = props => {
     return (
-        <h2>Crochet</h2>
+        <Container>
+            <Row xs={2} md={4} lg={6}>
+                {content.map(item => <ContentCard content={item} />)}
+            </Row>
+        </Container>
     )
 }
 
